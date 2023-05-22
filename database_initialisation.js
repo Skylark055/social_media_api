742d164f884a2a3754099892337e59a9d670c9ef
Kasper Steverink <whitehorsekart@gmail.com>
2023-05-22 15:05:05 +0200

    modified:   database_initialisation.js

--- database_initialisation.js
+++ database_initialisation.js
@@ -7,45 +7,82 @@ var con = mysql.createConnection({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
-}).promise();
+});
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 
-    const userBase = `CREATE TABLE userBase (id integer PRIMARY KEY AUTO_INCREMENT, email VARCHAR(255) NOT NULL, 
-               username VARCHAR(32) NOT NULL, password BINARY(64) NOT NULL, first_name VARCHAR(255) NOT NULL, 
-               last_name VARCHAR(255) NOT NULL, created TIMESTAMP NOT NULL DEFAULT NOW()`;
+    const userBase = `CREATE TABLE userBase (
+      user_ID integer PRIMARY KEY AUTO_INCREMENT, 
+      email VARCHAR(255) NOT NULL, 
+      username VARCHAR(32) NOT NULL, 
+      password BINARY(64) NOT NULL, 
+      first_name VARCHAR(255) NOT NULL, 
+      last_name VARCHAR(255) NOT NULL, 
+      created TIMESTAMP NOT NULL DEFAULT NOW()
+  )`;
     
     con.query(userBase, function (err, result) {
       if (err) throw err;
-      console.log("Table created");
+      console.log("Table userBase created");
     });
 
-    const posts = `CREATE TABLE posts (id integer PRIMARY KEY AUTO_INCREMENT, author_ID integer NOT NULL, 
-                               image MEDIUMBLOB, PATH_TO_CONTENT, description TEXT(160) NOT NULL, likes_ID integer NOT NULL, 
-                               comments_ID integer NOT NULL, created TIMESTAMP NOT NULL DEFAULT NOW())`;
-    con.query(posts, function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-      });
-    
-    const comments = "CREATE TABLE comments (id integer PRIMARY KEY AUTO_INCREMENT, comments)";
-    con.query(comments, function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-      });
+    const posts = `CREATE TABLE posts (
+      post_ID integer PRIMARY KEY AUTO_INCREMENT, 
+      author_ID integer NOT NULL, 
+      image MEDIUMBLOB, 
+      description TEXT(160), 
+      tot_likes integer NOT NULL, 
+      created TIMESTAMP NOT NULL DEFAULT NOW(),
+      FOREIGN KEY (author_ID) REFERENCES userBase(user_ID)
+  )`;
+  con.query(posts, function (err, result) {
+      if (err) throw err;
+      console.log("Table posts created");
+    });
 
-    const likes = "CREATE TABLE likes (id integer PRIMARY KEY AUTO_INCREMENT, )";
+    const likes = `CREATE TABLE likes (
+      like_ID integer PRIMARY KEY AUTO_INCREMENT,
+      post_ID integer NOT NULL,
+      author_ID integer NOT NULL, 
+      created TIMESTAMP NOT NULL DEFAULT NOW(), 
+      FOREIGN KEY (author_ID) REFERENCES userBase(user_ID),
+      FOREIGN KEY (post_ID) REFERENCES posts(post_ID)
+  )`;
+
     con.query(likes, function (err, result) {
         if (err) throw err;
-        console.log("Table created");
+        console.log("Table likes created");
       });
     
-    const friends = "CREATE TABLE friends (id integer PRIMARY KEY AUTO_INCREMENT, )";
+    const comments = `CREATE TABLE comments (
+      comments_ID integer PRIMARY KEY AUTO_INCREMENT, 
+      author_ID integer, 
+      content TEXT(160), 
+      post_ID integer, 
+      created TIMESTAMP NOT NULL DEFAULT NOW(),
+      FOREIGN KEY (author_ID) REFERENCES userBase(user_ID), 
+      FOREIGN KEY (post_ID) REFERENCES posts(post_ID)
+  )`;
+
+    con.query(comments, function (err, result) {
+        if (err) throw err;
+        console.log("Table comments created");
+      });
+    
+    const friends = `CREATE TABLE friends (
+      ID integer PRIMARY KEY,
+      friend_ID integer, 
+      profile_ID integer, 
+      created TIMESTAMP NOT NULL DEFAULT NOW(),
+      CONSTRAINT fk_friend_ID FOREIGN KEY (friend_ID) REFERENCES userBase(user_ID),
+      CONSTRAINT fk_profile_ID FOREIGN KEY (profile_ID) REFERENCES userBase(user_ID)
+  )`;
     con.query(friends, function (err, result) {
         if (err) throw err;
-        console.log("Table created");
+        console.log("Table friends created");
     });
+    console.log("Completed the set up")
 });
 
