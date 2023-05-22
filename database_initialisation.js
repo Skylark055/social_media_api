423f7c11508dd96b1b2e84a70850e65345cc8cf3
Kasper Steverink <whitehorsekart@gmail.com>
2023-05-22 15:50:12 +0200

    final init file

--- database_initialisation.js
+++ database_initialisation.js
@@ -2,17 +2,6 @@ import mysql from 'mysql2'
 import dotenv from 'dotenv'
 dotenv.config()
 
-var init = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  const create_DB = ``
-
-
 var con = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
